1e12bbcf9036aa6d964ceae9ab352e0586b9f2f4
phischu <pschuster@uni-koblenz.de>
2014-08-17 22:02:30 +0200

    signatures

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,7 @@ import Prelude hiding (writeFile)
 
 import Language.Haskell.Exts.Syntax (
     Module(Module),ModuleName(ModuleName),ModulePragma(LanguagePragma),
-    Name(Ident,Symbol),ImportDecl(ImportDecl),ImportSpec(IVar))
+    Name(Ident,Symbol),ImportDecl(ImportDecl),ImportSpec(IVar,IAbs))
 import Language.Haskell.Exts.SrcLoc (noLoc)
 import Language.Haskell.Exts.Parser (parseDecl,fromParseResult)
 import Language.Haskell.Exts.Pretty (prettyPrint)
@@ -18,41 +18,55 @@ import System.Process (rawSystem)
 
 import Control.Monad (forM_)
 
-data Fragment = Fragment FragmentID SourceCode [Usage]
+data Fragment = Fragment FragmentID FragmentDeclaration [Usage]
 
-data Usage = Usage (Maybe Qualification) Name SymbolSource
+data FragmentDeclaration = Binding Signature SourceCode
 
-data SymbolSource = OtherFragment FragmentID
-                  | Primitive OriginalModule
+data Usage = Usage (Maybe Qualification) UsedName Reference
+
+data UsedName = Variable Name
+              | Abstract Name
+
+data Reference = OtherFragment FragmentID
+               | Primitive OriginalModule
 
 type FragmentID = Integer
 type SourceCode = Text
 type Qualification = Text
 type OriginalModule = Text
+type Signature = SourceCode
 
 get :: FragmentID -> IO Fragment
-get 0 = return (Fragment 0 "main = putHello \"Fragnix!\""  [putHelloUsage]) where
-    putHelloUsage = Usage Nothing (Ident "putHello") (OtherFragment 1)
-get 1 = return (Fragment 1 "putHello x = putStrLn (\"Hello \" ++ x)" [putStrLnUsage,appendUsage]) where
-    putStrLnUsage = Usage Nothing (Ident "putStrLn") (Primitive "System.IO")
-    appendUsage = Usage Nothing (Symbol "++") (Primitive "Data.List")
+get 0 = return (Fragment 0 (Binding "main :: IO ()" "main = putHello \"Fragnix!\"")  [putHelloUsage,ioUsage]) where
+    putHelloUsage = Usage Nothing (Variable (Ident "putHello")) (OtherFragment 1)
+    ioUsage = Usage Nothing (Abstract (Ident "IO")) (Primitive "System.IO")
+get 1 = return (Fragment 1 (Binding "putHello :: String -> IO ()" "putHello x = putStrLn (\"Hello \" ++ x)") usages) where
+    usages = [putStrLnUsage,appendUsage,stringUsage,ioUsage]
+    putStrLnUsage = Usage Nothing (Variable (Ident "putStrLn")) (Primitive "System.IO")
+    appendUsage = Usage Nothing (Variable (Symbol "++")) (Primitive "Data.List")
+    stringUsage = Usage Nothing (Abstract (Ident "String")) (Primitive "Data.String")
+    ioUsage = Usage Nothing (Abstract (Ident "IO")) (Primitive "System.IO")
 
 assemble :: Fragment -> Module
-assemble (Fragment fragmentID sourceCode usages) =
-    let decl = fromParseResult (parseDecl (unpack sourceCode))
+assemble (Fragment fragmentID fragmentDeclaration usages) =
+    let decl = fromParseResult . parseDecl . unpack
+        decls = case fragmentDeclaration of
+            Binding signature body -> [decl signature,decl body]
         modulName = ModuleName (fragmentModuleName fragmentID)
         pragmas = [LanguagePragma noLoc [Ident "NoImplicitPrelude"]]
         imports = map usageImport usages
-    in Module noLoc modulName pragmas Nothing Nothing imports [decl]
+    in Module noLoc modulName pragmas Nothing Nothing imports decls
 
 usageImport :: Usage -> ImportDecl
-usageImport (Usage maybeQualification name symbolSource) =
+usageImport (Usage maybeQualification usedName symbolSource) =
     let modulName = case symbolSource of
             OtherFragment fragmentID -> ModuleName (fragmentModuleName fragmentID)
             Primitive originalModule -> ModuleName (unpack originalModule)
         qualified = maybe False (const True) maybeQualification
         maybeAlias = fmap (ModuleName . unpack) maybeQualification
-        importSpec = IVar name
+        importSpec = case usedName of
+            Variable name -> IVar name
+            Abstract name -> IAbs name
     in ImportDecl noLoc modulName qualified False Nothing maybeAlias (Just (False,[importSpec]))
 
 fragmentPath :: FragmentID -> FilePath
