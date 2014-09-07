c4481661a09aa8e4b984c8059e2263bd0cf0a97e
phischu <pschuster@uni-koblenz.de>
2014-09-07 18:11:14 +0200

    fix confusion in tests

--- tests/Tests.hs
+++ tests/Tests.hs
@@ -20,41 +20,41 @@ main = do
 writeSliceTests :: TestTree
 writeSliceTests = testGroup "writeSlice" [
     goldenVsFile
-        "putHelloSlice"
-        "tests/examples/writeSliceputHelloSlice.golden"
-        "fragnix/slices/0"
-        (writeSlice putHelloSlice),
-    goldenVsFile
         "mainSlice"
         "tests/examples/writeSlicemainSlice.golden"
+        "fragnix/slices/0"
+        (writeSlice mainSlice),
+    goldenVsFile
+        "putHelloSlice"
+        "tests/examples/writeSliceputHelloSlice.golden"
         "fragnix/slices/1"
-        (writeSlice mainSlice)]
+        (writeSlice putHelloSlice)]
 
 readSliceTests :: TestTree
 readSliceTests = testGroup "readSlice" [
     goldenVsFile
-        "putHelloSlice"
-        "tests/examples/readSliceputHelloSlice.golden"
-        "tests/examples/readSliceputHelloSlice.out"
-        (readSlice 0 >>= writeFile "tests/examples/readSliceputHelloSlice.out" . show),
-    goldenVsFile
         "mainSlice"
         "tests/examples/readSlicemainSlice.golden"
         "tests/examples/readSlicemainSlice.out"
-        (readSlice 1 >>= writeFile "tests/examples/readSlicemainSlice.out" . show)]
+        (readSlice 0 >>= writeFile "tests/examples/readSlicemainSlice.out" . show),
+    goldenVsFile
+        "putHelloSlice"
+        "tests/examples/readSliceputHelloSlice.golden"
+        "tests/examples/readSliceputHelloSlice.out"
+        (readSlice 1 >>= writeFile "tests/examples/readSliceputHelloSlice.out" . show)]
 
 writeSliceModuleTests :: TestTree
 writeSliceModuleTests = testGroup "writeSliceModule" [
     goldenVsFile
-        "putHelloSlice"
-        "tests/examples/writeSliceModuleputHelloSlice.golden"
-        "fragnix/modules/F0.hs"
-        (writeSliceModule putHelloSlice),
-    goldenVsFile
         "mainSlice"
         "tests/examples/writeSliceModulemainSlice.golden"
+        "fragnix/modules/F0.hs"
+        (writeSliceModule mainSlice),
+    goldenVsFile
+        "putHelloSlice"
+        "tests/examples/writeSliceModuleputHelloSlice.golden"
         "fragnix/modules/F1.hs"
-        (writeSliceModule mainSlice)]
+        (writeSliceModule putHelloSlice)]
 
 compileTest :: TestTree
 compileTest = testGroup "compile" [
@@ -64,13 +64,13 @@ compileTest = testGroup "compile" [
         "tests/examples/compile.out"
         (compile 0 >>= writeFile "tests/examples/compile.out" . show)]
 
-putHelloSlice :: Slice
-putHelloSlice = Slice 0 (Binding "main :: IO ()" "main = putHello \"Fragnix!\"")  [putHelloUsage,ioUsage] where
+mainSlice :: Slice
+mainSlice = Slice 0 (Binding "main :: IO ()" "main = putHello \"Fragnix!\"")  [putHelloUsage,ioUsage] where
     putHelloUsage = Usage Nothing (VarId "putHello") (OtherSlice 1)
     ioUsage = Usage Nothing (ConId "IO") (Primitive "System.IO")
 
-mainSlice :: Slice
-mainSlice = Slice 1 (Binding "putHello :: String -> IO ()" "putHello x = putStrLn (\"Hello \" ++ x)") usages where
+putHelloSlice :: Slice
+putHelloSlice = Slice 1 (Binding "putHello :: String -> IO ()" "putHello x = putStrLn (\"Hello \" ++ x)") usages where
     usages = [putStrLnUsage,appendUsage,stringUsage,ioUsage]
     putStrLnUsage = Usage Nothing (VarId "putStrLn") (Primitive "System.IO")
     appendUsage = Usage Nothing (VarSym "++") (Primitive "Data.List")
