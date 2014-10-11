9da0a519029a7e833ecb3c6fea93b637803f1579
phischu <pschuster@uni-koblenz.de>
2014-10-11 09:59:54 +0200

    package declarations

---
+++ src/package-declarations.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import System.Process (rawSystem)
+
+main :: IO ()
+main = do
+    let packagequalifier = "containers"
+    exitCode <- rawSystem "cabal" [
+        "install","--force-reinstalls",
+        "--package-db=fragnix/builtin/packages.db",
+        "--package-db=fragnix/names/packages.db",
+        "--prefix=fragnix/names/",
+        "--gcc-option=-I/usr/lib/ghc/include",
+        "--haskell-suite","-w",".cabal-sandbox/bin/haskell-declarations",
+        packagequalifier]
+    print exitCode
