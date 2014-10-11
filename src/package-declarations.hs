9780ebbc0970a5a75efba527506127275d78e21e
phischu <pschuster@uni-koblenz.de>
2014-10-11 10:46:13 +0200

    package declarations

--- src/package-declarations.hs
+++ src/package-declarations.hs
@@ -2,16 +2,21 @@
 module Main where
 
 import System.Process (rawSystem)
+import System.Directory (createDirectoryIfMissing)
 
 main :: IO ()
 main = do
     let packagequalifier = "containers"
+    createDirectoryIfMissing True "fragnix/names/"
+    writeFile "fragnix/names/packages.db" "[]"
     exitCode <- rawSystem "cabal" [
         "install","--force-reinstalls",
         "--package-db=fragnix/builtin/packages.db",
         "--package-db=fragnix/names/packages.db",
-        "--prefix=fragnix/names/",
+        "--prefix=/home/pschuster/Projects/fragnix-test/fragnix/names/",
+        "--libdir=$prefix",
+        "--libsubdir=$pkgid",
         "--gcc-option=-I/usr/lib/ghc/include",
-        "--haskell-suite","-w",".cabal-sandbox/bin/module-declarations",
+        "--haskell-suite","-w","module-declarations",
         packagequalifier]
     print exitCode
