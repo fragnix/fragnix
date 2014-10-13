02b85940f928adf556d98345f685f75ea366cd06
phischu <pschuster@uni-koblenz.de>
2014-10-13 13:29:30 +0200

    build scc graph separately

--- src/Fragnix/DeclarationSlices.hs
+++ src/Fragnix/DeclarationSlices.hs
@@ -8,7 +8,7 @@ import Fragnix.Slice (
 import Language.Haskell.Names (
     SymValueInfo,SymTypeInfo,OrigName,Symbols)
 
-import Data.Graph.Inductive (Node,Context,buildGr,scc,lab,lsuc)
+import Data.Graph.Inductive (Node,Context,buildGr,scc,lsuc,labNodes)
 import Data.Graph.Inductive.PatriciaTree (Gr)
 
 import Data.Text (pack)
@@ -16,25 +16,8 @@ import qualified Data.Map as Map (lookup,fromList)
 import Data.Maybe (maybeToList)
 
 declarationSlices :: [Declaration] -> [Slice]
-declarationSlices declarations = map (buildSlice declarationgraph) (scc declarationgraph) where
+declarationSlices declarations = buildSlices (sccGraph declarationgraph (scc declarationgraph)) where
     declarationgraph = buildGr (contexts (zip [0..] declarations)) :: Gr Declaration Symbol
-
-buildSlice :: Gr Declaration Symbol -> [Node] -> Slice
-buildSlice declarationgraph nodes = Slice tempID fragments usages where
-    tempID = fromIntegral (head nodes)
-    fragments = Fragment (do
-        node <- nodes
-        Declaration _ ast _ _ <- maybeToList (lab declarationgraph node)
-        return (pack ast))
-    usages = do
-        node <- nodes
-        Declaration _ _ _ symbols <- maybeToList (lab declarationgraph node)
-        symbol <- listSymbols symbols
-        let usedname = symbolName symbol
-            reference = case lookup symbol (map (\(x,y) -> (y,x)) (lsuc declarationgraph node)) of
-                Nothing -> Primitive undefined
-                Just othernode -> OtherSlice (fromIntegral othernode)
-        return (Usage Nothing usedname reference)
 
 contexts :: [(Node,Declaration)] -> [Context Declaration Symbol]
 contexts declarationnodes = do
@@ -45,6 +28,29 @@ contexts declarationnodes = do
             return (mentionedsymbol,useddeclaration)
     return ([],node,declaration,useddeclarations) where
         boundMap = Map.fromList undefined
+
+sccGraph :: Gr Declaration Symbol -> [[Node]] -> Gr [Declaration] Symbol
+sccGraph = undefined
+
+buildSlices :: Gr [Declaration] Symbol -> [Slice]
+buildSlices sccgraph = do
+    (node,declarations) <- labNodes sccgraph
+    let tempID = fromIntegral node
+        fragments = Fragment (do
+            Declaration _ ast _ _ <- declarations
+            return (pack ast))
+        usages = do
+            Declaration _ _ _ symbols <- declarations
+            symbol <- listSymbols symbols
+            let usedname = symbolName symbol
+                reference = case lookup symbol (map (\(x,y) -> (y,x)) (lsuc sccgraph node)) of
+                    Nothing -> Primitive undefined
+                    Just othernode -> OtherSlice (fromIntegral othernode)
+            return (Usage Nothing usedname reference)
+    return (Slice tempID fragments usages)
+
+hashSlices :: [Slice] -> [Slice]
+hashSlices = id
 
 listSymbols :: Symbols -> [Symbol]
 listSymbols = undefined
