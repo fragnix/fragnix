d886567681ff2184ddff757486f0446862d3655a
phischu <pschuster@uni-koblenz.de>
2014-11-20 12:28:09 +0100

    split testing into its own exe

--- src/fragnix-test.hs
+++ src/fragnix-test.hs
@@ -12,16 +12,19 @@ import Control.Monad (forM_,forM)
 import System.Environment (getArgs)
 import System.Exit (ExitCode(ExitSuccess,ExitFailure))
 
-fragnixExecutable :: IO ()
-fragnixExecutable = do
+fragnixTest :: IO ()
+fragnixTest = do
     args <- getArgs
     declarations <- modulDeclarations args
     writeDeclarations "fragnix/declarations/declarations.json" declarations
-    let (slices,globalscope) = declarationSlices declarations
+    let (slices,_) = declarationSlices declarations
     forM_ slices writeSlice
-    case Map.lookup mainsymbol globalscope of
-        Nothing -> putStrLn "No main slice!"
-        Just mainSliceID -> sliceCompilerMain mainSliceID >>= print
+    let sliceIDs = [sliceID | Slice sliceID _ _ _ <- slices]
+    exitCodes <- forM sliceIDs (\sliceID -> sliceCompiler sliceID)
+    let successes = length [() | ExitSuccess <- exitCodes]
+        failures = length [() | ExitFailure _ <- exitCodes]
+    putStrLn ("Successes: " ++ show successes)
+    putStrLn ("Failures: " ++ show failures)
 
 main :: IO ()
-main = fragnixExecutable
+main = fragnixTest
