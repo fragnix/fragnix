bdf3011b323dd5f962b11707d268e914b51270e8
phischu <pschuster@uni-koblenz.de>
2014-12-15 17:02:33 +0100

    tests for text package

---
+++ tests/packages/text/Data.Text.Lazy.Internal.hs
@@ -0,0 +1,139 @@
+{-# LINE 1 "./Data/Text/Lazy/Internal.hs" #-}
+{-# LINE 1 "dist/dist-sandbox-235ea54e/build/autogen/cabal_macros.h" #-}
+                                                                
+
+                           
+
+
+
+
+
+
+                          
+
+
+
+
+
+
+                                 
+
+
+
+
+
+
+                             
+
+
+
+
+
+
+                              
+
+
+
+
+
+
+                                    
+
+
+
+
+
+
+                     
+
+
+
+
+
+
+                       
+
+
+
+
+
+
+                  
+
+
+
+
+
+
+                    
+
+
+
+
+
+
+                        
+
+
+
+
+
+
+                         
+
+
+
+
+
+
+                       
+
+
+
+
+
+
+                   
+
+
+
+
+
+
+                      
+
+
+
+
+
+
+                          
+
+
+
+
+
+
+
+{-# LINE 2 "./Data/Text/Lazy/Internal.hs" #-}
+{-# LINE 1 "./Data/Text/Lazy/Internal.hs" #-}
+{-# LANGUAGE BangPatterns, DeriveDataTypeable #-}
+-- |
+-- Module      : Data.Text.Lazy.Internal
+-- Copyright   : (c) 2013 Bryan O'Sullivan
+--
+-- License     : BSD-style
+-- Maintainer  : bos@serpentine.com
+-- Stability   : experimental
+-- Portability : GHC
+--
+-- This module has been renamed to 'Data.Text.Internal.Lazy'. This
+-- name for the module will be removed in the next major release.
+
+module Data.Text.Lazy.Internal
+    {-# DEPRECATED "Use Data.Text.Internal.Lazy instead" #-}
+    (
+      module Data.Text.Internal.Lazy
+    ) where
+
+import Data.Text.Internal.Lazy
