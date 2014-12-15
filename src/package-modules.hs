72e354a8ea1b59dea2fca2f0288c306c857fbd5e
phischu <pschuster@uni-koblenz.de>
2014-12-15 16:26:53 +0100

    copy module files after package installation

--- src/package-modules.hs
+++ src/package-modules.hs
@@ -3,8 +3,11 @@ module Main where
 
 import System.Process (rawSystem)
 import System.Environment (getArgs)
-import System.Directory (getCurrentDirectory)
-import System.FilePath ((</>))
+import System.Directory (
+    getCurrentDirectory,getDirectoryContents,
+    createDirectoryIfMissing,copyFile)
+import System.FilePath ((</>),takeExtension)
+import Control.Monad (forM_)
 
 main :: IO ()
 main = do
@@ -17,4 +20,23 @@ main = do
         "--prefix=" ++ (currentdirectory </> "fragnix" </> "temp"),
         "--package-db=" ++ (currentdirectory </> "fragnix" </> "temp" </> "packages.db"),
         packagequalifier]
+    copyModuleFiles
     print exitCode
+
+copyModuleFiles :: IO ()
+copyModuleFiles = do
+    libraryFolderNames <- getDirectoryContents installationPath >>= return . filter (not . (=='.') . head)
+    createDirectoryIfMissing True moduleSourcePath
+    forM_ libraryFolderNames (\libraryFolderName -> do
+        moduleFileNames <- getDirectoryContents (installationPath </> libraryFolderName) >>= return . filter ((==".hs") . takeExtension)
+        forM_ moduleFileNames (\moduleFileName -> do
+            copyFile
+                (installationPath </> libraryFolderName </> moduleFileName)
+                (moduleSourcePath </> moduleFileName)))
+
+
+installationPath :: FilePath
+installationPath = "fragnix" </> "temp" </> "lib" </> "x86_64-linux-haskell-modules-0.1"
+
+moduleSourcePath :: FilePath
+moduleSourcePath = "fragnix" </> "temp" </> "modulesources"
