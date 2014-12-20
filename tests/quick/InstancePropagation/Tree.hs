8b83dba50fd8a4921c65278fad029a6bc581d2c9
phischu <pschuster@uni-koblenz.de>
2014-12-20 14:24:07 +0100

    Simpler instance propagation test

--- tests/quick/InstancePropagation/Tree.hs
+++ tests/quick/InstancePropagation/Tree.hs
@@ -2,10 +2,10 @@
 module Tree where
 
 import Data.Data (Data)
+import Data.Typeable (Typeable)
 
-import {-# SOURCE #-} TypeableTree ()
 
 data Tree a = Node{rootLabel :: a, subForest :: Forest a}
-            deriving (Eq, Read, Show, Data)
+            deriving (Eq, Read, Show, Data, Typeable)
 
 type Forest a = [Tree a]
--- tests/quick/InstancePropagation/TypeableTree.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE  DeriveDataTypeable, StandaloneDeriving #-}
-module TypeableTree where
-
-import Data.Typeable (Typeable)
-import Tree (Tree)
-
-deriving instance Typeable Tree
