a64e89f2e7f79d0206617982b5a18cc07a52d05a
phischu <pschuster@uni-koblenz.de>
2015-01-18 23:12:51 +0100

    make environment imports`

--- src/Fragnix/Environment.hs
+++ src/Fragnix/Environment.hs
@@ -1,17 +1,31 @@
 module Fragnix.Environment where
 
-import Fragnix.Slice (Reference(Primitive))
+import Fragnix.Slice (
+    Reference(Primitive))
 
-import Language.Haskell.Names (Symbol(Value))
-import Language.Haskell.Exts (ModuleName(ModuleName),Name(Ident),prettyPrint)
+import Language.Haskell.Names (
+    Symbol(Value))
+import Language.Haskell.Names.Interfaces (
+    readInterface,writeInterface)
+import Language.Haskell.Exts (
+    ModuleName(ModuleName),Name(Ident),prettyPrint)
 
-import Data.Map (Map)
-import qualified Data.Map as Map (fromList,toList,empty)
-import System.FilePath ((</>),dropFileName)
-import System.Directory (createDirectoryIfMissing,doesFileExist)
-import Data.Aeson (eitherDecode,encode)
-import Data.Text (pack)
-import qualified Data.ByteString.Lazy as ByteString (readFile,writeFile)
+import Data.Map (
+    Map)
+import qualified Data.Map as Map (
+    fromList,toList,empty)
+import System.FilePath (
+    (</>),dropFileName)
+import System.Directory (
+    createDirectoryIfMissing,doesFileExist,getDirectoryContents)
+import Data.Aeson (
+    eitherDecode,encode)
+import Data.Text (
+    pack)
+import qualified Data.ByteString.Lazy as ByteString (
+    readFile,writeFile)
+import Control.Monad (
+    filterM,forM,forM_)
 
 type Environment = Map ModuleName [Symbol]
 
@@ -21,14 +35,14 @@ loadEnvironment path = do
     filenames <- getDirectoryContents path
     let pathmodulnames = map (\filename -> (ModuleName filename,path </> filename)) filenames
     existingPathModulNames <- filterM (doesFileExist . snd) pathmodulnames
-    fmap fromList (forM existingPathModulNames (\(modulname,modulpath) -> do
+    fmap Map.fromList (forM existingPathModulNames (\(modulname,modulpath) -> do
         symbols <- readInterface modulpath
         return (modulname,symbols)))
 
 persistEnvironment :: FilePath -> Environment -> IO ()
 persistEnvironment path environment = do
     createDirectoryIfMissing True path
-    forM_ (toList environment) (\(modulname,symbols) -> do
+    forM_ (Map.toList environment) (\(modulname,symbols) -> do
         let modulpath = path </> prettyPrint modulname
         writeInterface modulpath symbols)
 
