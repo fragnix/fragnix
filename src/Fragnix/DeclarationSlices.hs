5dc6095e1b16ca04e568768f2cd952549fa3817f
phischu <pschuster@uni-koblenz.de>
2015-01-19 13:31:56 +0100

    Refactor declarationSlices

--- src/Fragnix/DeclarationSlices.hs
+++ src/Fragnix/DeclarationSlices.hs
@@ -12,17 +12,18 @@ import Language.Haskell.Names (
 import qualified Language.Haskell.Exts as Name (
     Name(Ident,Symbol))
 import Language.Haskell.Exts (
-    ModuleName,prettyExtension,Name,prettyPrint,
+    ModuleName(ModuleName),prettyExtension,Name,prettyPrint,
     Extension(EnableExtension),KnownExtension(Safe,CPP,Trustworthy))
 
 import Data.Graph.Inductive (
-    buildGr,scc,lab,lsuc,labNodes,insEdges,insNodes,empty)
+    Node,buildGr,scc,lab,lsuc,labNodes,insEdges,insNodes,empty)
 import Data.Graph.Inductive.PatriciaTree (
     Gr)
 
 import Control.Monad (guard)
 import Control.Applicative ((<|>))
 import Data.Text (pack)
+import Data.Char (isDigit)
 import Data.Map (Map)
 import qualified Data.Map as Map (lookup,fromList,fromListWith)
 import qualified Data.Set as Set (fromList,member)
@@ -33,8 +34,48 @@ import Data.List (nub,(\\))
 
 -- | Extract all slices from the given list of declarations.
 declarationSlices :: [Declaration] -> [Slice]
-declarationSlices declarations =
-    hashSlices (buildTempSlices (sccGraph (declarationGraph declarations)))
+declarationSlices declarations = slices where
+    sccs = labNodes (sccGraph (declarationGraph declarations))
+    sliceBindingsMap = sliceBindings sccs
+    sliceInstancesMap = sliceInstances sccs
+    slices = hashSlices (map (buildTempSlice sliceBindingsMap sliceInstancesMap) sccs)
+
+
+-- | Build a Map from symbol to Node that binds this symbol.
+sliceBindings :: [(Node,[Declaration])] -> Map Symbol Node
+sliceBindings fragmentNodes = Map.fromList (do
+    (node,declarations) <- fragmentNodes
+    Declaration _ _ _ boundsymbols _ <- declarations
+    boundsymbol <- boundsymbols
+    return (boundsymbol,node))
+
+
+-- | Build a Map from data or class symbol to instance temporary ID
+-- Prefer the class symbol if both are present
+-- Only include classes and types bound in this graph
+sliceInstances :: [(Node,[Declaration])] -> Map Symbol [Node]
+sliceInstances fragmentNodes = Map.fromListWith (++) (do
+
+    let graphSymbols = Set.fromList (do
+            (_,declarations) <- fragmentNodes
+            Declaration _ _ _ boundSymbols _ <- declarations
+            boundSymbols)
+
+    (node,declarations) <- fragmentNodes
+    Declaration ClassInstance _ _ _ mentionedsymbols <- declarations
+
+    let classSymbol = do
+            classSymbolMentionedLast <- listToMaybe (reverse (filter isClass (map fst mentionedsymbols)))
+            guard (Set.member classSymbolMentionedLast graphSymbols)
+            return classSymbolMentionedLast
+        typeSymbol = do
+            typeSymbolMentionedFirst <- listToMaybe (filter isType (map fst mentionedsymbols))
+            guard (Set.member typeSymbolMentionedFirst graphSymbols)
+            return typeSymbolMentionedFirst
+    symbol <- maybeToList (classSymbol <|> typeSymbol)
+
+    return (symbol,[node]))
+
 
 -- | Create a dependency graph between all declarations in the given list. Dependency edges
 -- come primarily from when a declaration mentions a symbol another declaration binds. But also
@@ -66,6 +107,7 @@ declarationGraph declarations =
         bindingnode <- maybeToList (Map.lookup mentionedsymbol boundmap)
         return (bindingnode,fixitynode,Fixity)
 
+
 -- | Build a graph of strongly connected components from a given graph.
 sccGraph :: Gr a b -> Gr [a] b
 sccGraph graph = buildGr (do
@@ -84,38 +126,15 @@ sccGraph graph = buildGr (do
             return (label,sccsuc)
     return ([],sccnode,scclabels,sccsucs))
 
+
 -- | Take a graph where each node corresponds to a
 -- list of declarations that from a strongly connected component. Return a list of slices
 -- tripled with the symbols it binds and the mentioned symbols that could not be resolved
 -- inside the graph. The slices have negative IDs starting from -1.
-buildTempSlices :: Gr [Declaration] Dependency -> [Slice]
-buildTempSlices tempslicegraph = do
-
-    -- Build a Map from data or class symbol to instance temporary ID
-    -- Prefer the class symbol if both are present
-    -- Only include classes and types bound in this graph
-    let graphSymbols = Set.fromList (do
-            (_,declarations) <- labNodes tempslicegraph
-            Declaration _ _ _ boundSymbols _ <- declarations
-            boundSymbols)
-        instanceMap = Map.fromListWith (++) (do
-            (node,declarations) <- labNodes tempslicegraph
-            Declaration ClassInstance _ _ _ mentionedsymbols <- declarations
-            let classSymbol = do
-                    classSymbolMentionedLast <- listToMaybe (reverse (filter isClass (map fst mentionedsymbols)))
-                    guard (Set.member classSymbolMentionedLast graphSymbols)
-                    return classSymbolMentionedLast
-                typeSymbol = do
-                    typeSymbolMentionedFirst <- listToMaybe (filter isType (map fst mentionedsymbols))
-                    guard (Set.member typeSymbolMentionedFirst graphSymbols)
-                    return typeSymbolMentionedFirst
-            symbol <- maybeToList (classSymbol <|> typeSymbol)
-            return (symbol,[node]))
-
-    -- for all nodes
-    (node,declarations) <- labNodes tempslicegraph
-
-    let tempID = fromIntegral node
+buildTempSlice :: Map Symbol Node -> Map Symbol [Node] -> (Node,[Declaration]) -> Slice
+buildTempSlice tempEnvironment instanceMap (node,declarations) =
+    Slice tempID language fragments (mentionedUsages ++ instanceUsages) where
+        tempID = fromIntegral node
 
         language = Language (nub (do
             Declaration _ ghcextensions _ _ _ <- declarations
@@ -128,37 +147,35 @@ buildTempSlices tempslicegraph = do
             Declaration _ _ ast _ _ <- arrange declarations
             return ast)
 
-        otherslices = Map.fromList (do
-            (otherSliceTempID,UsesSymbol _ symbol) <- lsuc tempslicegraph node
-            return (symbol,OtherSlice (fromIntegral otherSliceTempID)))
-
-        locallyBoundSymbols = do
-            Declaration _ _ _ boundsymbols _ <- declarations
-            boundsymbols
-
         -- A usage for every mentioned symbol
         mentionedUsages = nub (do
             Declaration _ _ _ _ mentionedsymbols <- declarations
             (mentionedsymbol,maybequalification) <- mentionedsymbols
+
             let maybeQualificationText = fmap (pack . prettyPrint) maybequalification
                 usedName = symbolUsedName mentionedsymbol
-            -- No usage for declarations in this fragment
-            guard (not (mentionedsymbol `elem` locallyBoundSymbols))
-            -- Look up reference to other slice from this graph
-            -- if it fails the symbol must be builtin
-            reference <- maybeToList (
-                    Map.lookup mentionedsymbol otherslices <|>
-                    Just (Primitive (pack (prettyPrint (symbolModule (mentionedsymbol))))))
+                -- Look up reference to other slice from this graph
+                -- if it fails the symbol must be from the environment
+                maybeReference = case Map.lookup mentionedsymbol tempEnvironment of
+                    -- If the symbol is from the environment it is either builtin or refers to an existing slice
+                    Nothing -> Just (moduleReference (symbolModule (mentionedsymbol)))
+                    -- If the symbol is from this fragment we generate no reference
+                    Just referenceNode -> if referenceNode == node
+                        then Nothing
+                        else Just (OtherSlice (fromIntegral referenceNode))
+                
+            reference <- maybeToList maybeReference
+
             return (Usage maybeQualificationText usedName reference))
 
         -- We want every class to import all its instances
         -- For builtin classes we want the data type to import the instances
         instanceUsages = do
-            boundSymbol <- locallyBoundSymbols
+            Declaration _ _ _ boundSymbols _ <- declarations
+            boundSymbol <- boundSymbols
             instanceSliceTempID <- concat (maybeToList (Map.lookup boundSymbol instanceMap))
             return (Usage Nothing Instance (OtherSlice (fromIntegral instanceSliceTempID)))
 
-    return (Slice tempID language fragments (mentionedUsages ++ instanceUsages))
 
 -- | Arrange a list of declarations so that the signature is directly above the corresponding
 -- binding declaration
@@ -175,6 +192,15 @@ arrange declarations = arrangements ++ (declarations \\ arrangements) where
     signatures = do
         signature@(Declaration TypeSignature _ _ _ _) <- declarations
         return signature
+
+
+-- | We abuse module names to either refer to builtin modules or to a slice.
+-- If the module name refers to a slice it consists entirely of digits.
+moduleReference :: ModuleName -> Reference
+moduleReference (ModuleName moduleName)
+    | all isDigit moduleName = OtherSlice (read moduleName)
+    | otherwise = Primitive (pack moduleName)
+
 
 -- | A temporary ID before slices can be hashed.
 type TempID = Integer
