c2629ec912d7b44381813b0e3ffea97f71f06e0e
phischu <pschuster@uni-koblenz.de>
2015-02-07 12:13:02 +0100

    test foreign use of newtype of newtype

--- tests/quick/ForeignImport/ForeignImport.hs
+++ tests/quick/ForeignImport/ForeignImport.hs
@@ -3,6 +3,11 @@ import GHC.Ptr (Ptr)
 import GHC.Word (Word8)
 import System.IO (IO)
 import Foreign.C.Types (CInt(..),CSize(..))
+import System.Posix.Types (Fd(..),COff(..))
+import Data.Int (Int64)
 
 foreign import ccall unsafe "string.h memcpy" c_memcpy ::
                Ptr Word8 -> Ptr Word8 -> CSize -> IO (Ptr Word8)
+
+foreign import ccall unsafe "sendfile" c_sendfile ::
+               Fd -> Fd -> Ptr COff -> CSize -> IO (Int64)
