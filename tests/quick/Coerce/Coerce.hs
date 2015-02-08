d4ccfc5ded1438cb53db9aed1b22dccb55b50bd8
phischu <pschuster@uni-koblenz.de>
2015-02-08 13:32:01 +0100

    augment coerce test with case where constructor has name different from newtype

--- tests/quick/Coerce/Coerce.hs
+++ tests/quick/Coerce/Coerce.hs
@@ -1,8 +1,15 @@
 module Coerce where
 
 import Data.Coerce (coerce)
+import Data.Monoid (Monoid, (<>), mappend)
 
 newtype Elem a = Elem {getElem :: a}
 
 instance Functor Elem where
+    fmap = coerce 
+
+newtype WrappedMonoid m = WrapMonoid
+  { unwrapMonoid :: m }
+
+instance Functor WrappedMonoid where
     fmap = coerce 
