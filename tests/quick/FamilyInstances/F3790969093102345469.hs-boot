6f67b870cda9084e01175135278af452e67aec61
phischu <pschuster@uni-koblenz.de>
2015-06-05 21:26:15 +0200

    remove redundant test

--- tests/quick/FamilyInstances/F3790969093102345469.hs-boot
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE NoImplicitPrelude, DeriveDataTypeable,
-  FlexibleInstances, MultiParamTypeClasses, TypeFamilies,
-  ScopedTypeVariables, Rank2Types, NondecreasingIndentation,RoleAnnotations,
-  ExplicitForAll, PatternGuards #-}
-module F3790969093102345469 where
-import GHC.Show (Show)
-import {-# SOURCE #-} F903085878682639365 (Prim)
-import GHC.Show (showsPrec)
--- tests/quick/FamilyInstances/F8599798322733369681.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE NoImplicitPrelude, DeriveDataTypeable,
-  MultiParamTypeClasses, FlexibleInstances, ScopedTypeVariables,
-  NondecreasingIndentation, ExplicitForAll, PatternGuards #-}
-module F8599798322733369681 where
-import GHC.Types (Int)
-import Data.Typeable.Internal (Typeable)
-
-data MVector s a = MVector {-# UNPACK #-} !Int {-# UNPACK #-} !Int Int
-                 deriving Typeable
