e68696d2acd6b5972f1925d1713bb40c43101395
Philipp Schuster <phischu3000@googlemail.com>
2015-07-24 23:38:43 +0200

    reproduce error with type family in superclass constraint

--- tests/quick/FamilyInstances/FamilyInstances.hs
+++ tests/quick/FamilyInstances/FamilyInstances.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, FlexibleContexts #-}
 module FamilyInstances where
 
 data A = A
@@ -11,3 +11,15 @@ type instance State B = Bool
 
 f :: State B -> Bool
 f x = x
+
+class C a where
+    c :: a
+
+instance C Int where
+    c = 0
+
+class C (State a) => D a where
+    d :: a
+
+instance D A where
+    d = A
