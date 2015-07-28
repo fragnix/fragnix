0acb75db678dbf2dd747a672ebf22597850246c6
Philipp Schuster <phischu3000@googlemail.com>
2015-07-28 21:41:11 +0200

    delete failing test for same type different instance

--- tests/quick/SameTypeDifferentInstance/SameTypeDifferentInstance1.hs
+++
@@ -1,6 +0,0 @@
-module SameTypeDifferentInstance1 where
-
-data SameTypeDifferentInstance = SameTypeDifferentInstance
-
-instance Eq SameTypeDifferentInstance where
-    (==) SameTypeDifferentInstance SameTypeDifferentInstance = True
--- tests/quick/SameTypeDifferentInstance/SameTypeDifferentInstance2.hs
+++
@@ -1,6 +0,0 @@
-module SameTypeDifferentInstance2 where
-
-data SameTypeDifferentInstance = SameTypeDifferentInstance
-
-instance Eq SameTypeDifferentInstance where
-    (/=) SameTypeDifferentInstance SameTypeDifferentInstance = False
