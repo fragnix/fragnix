9ae3c3b8c02b52a71ec5a29d71713136a9a2ecbd
Philipp Schuster <phischu3000@googlemail.com>
2016-02-25 10:32:54 +0100

    More detailed benchmarking.

--- benchmarks-src/Main.hs
+++ benchmarks-src/Main.hs
@@ -13,16 +13,25 @@ import Fragnix.Slice (
     Slice, Language, Fragment, Use, Reference, UsedName, Name,
     Instance, InstancePart)
 
-import Criterion.Main (defaultMain, bench, nfIO, nf, whnfIO)
+import Criterion.Main (defaultMain, bgroup, bench, nfIO, nf, whnfIO)
 
-import Language.Haskell.Names (Symbol(..))
+import Language.Haskell.Names (
+    Symbol(..), Scoped(..), NameInfo(..), Error(..),
+    resolve, annotate)
 
-import Language.Haskell.Exts (ModuleName, Extension(..), KnownExtension(..))
+import Language.Haskell.Exts (
+    ModuleName, Extension(..), KnownExtension(..),
+    QName, SpecialCon, Boxed)
+import Language.Haskell.Exts.SrcLoc (
+    SrcSpan(..), SrcLoc(..))
 import qualified Language.Haskell.Exts as HaskellExts (Name)
+import qualified Language.Haskell.Exts.Annotated as Annotated (
+    Name, QName, ModuleName, SpecialCon)
 
 import Control.DeepSeq (NFData)
 import GHC.Generics (Generic)
 import Control.Monad (forM)
+import Data.Foldable (toList)
 import System.Directory (getDirectoryContents)
 
 
@@ -37,22 +46,42 @@ main = do
 
     modules <- forM modulePaths parse
 
+    let environment = resolve modules builtinEnvironment
+
     let declarations = moduleDeclarationsWithEnvironment builtinEnvironment modules
 
     defaultMain [
         bench "loadEnvironment" (nfIO (loadEnvironment builtinEnvironmentPath)),
         bench "parse" (whnfIO (forM modulePaths parse)),
-        bench "moduleDeclarations" (nf (moduleDeclarationsWithEnvironment builtinEnvironment) modules),
-        bench "declarationSlices" (nf (fst . declarationSlices) declarations)]
+        bgroup "moduleDeclarations" [
+          bench "moduleDeclarations" (nf (moduleDeclarationsWithEnvironment builtinEnvironment) modules),
+          bench "resolve" (nf (resolve modules) builtinEnvironment),
+          bench "annotate" (nf (map (toList . annotate environment)) modules)],
+        bench "declarationSlices" (nf declarationSlices declarations)]
 
 
 instance NFData ModuleName
 instance NFData Symbol
+
+instance (NFData a) => NFData (Scoped a)
+instance (NFData a) => NFData (NameInfo a)
+instance (NFData a) => NFData (Error a)
+instance (NFData a) => NFData (Annotated.Name a)
+instance (NFData a) => NFData (Annotated.QName a)
+instance (NFData a) => NFData (Annotated.ModuleName a)
+instance (NFData a) => NFData (Annotated.SpecialCon a)
+instance NFData SrcSpan
+instance NFData SrcLoc
+instance NFData QName
+instance NFData SpecialCon
+instance NFData Boxed
+
 instance NFData Declaration
 instance NFData Genre
 instance NFData HaskellExts.Name
 instance NFData Extension
 instance NFData KnownExtension
+
 instance NFData Slice
 instance NFData Language
 instance NFData Fragment
@@ -63,6 +92,10 @@ instance NFData Name
 instance NFData Instance
 instance NFData InstancePart
 
+deriving instance Generic (Error a)
+deriving instance Generic (NameInfo a)
+deriving instance Generic SrcSpan
+deriving instance Generic (Scoped a)
 deriving instance Generic KnownExtension
 deriving instance Generic Extension
 deriving instance Generic Symbol
