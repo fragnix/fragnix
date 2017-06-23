dd9a0536327129233f4f0b7060dc82db9bfa79d2
Philipp Schuster <philipp.schuster@uni-tuebingen.de>
2017-06-23 15:38:11 +0200

    Delete hs-boot files from base and update builtin environment.

--- utilities-src/create_builtin_environment.hs
+++ utilities-src/create_builtin_environment.hs
@@ -20,6 +20,24 @@ import Control.Monad (
   forM)
 
 
+{-
+
+Steps to extract preprocessed module files from base, ghc-prim and integer-gmp
+delete non-source files and folders
+Do those before invoking this executable
+
+find */ -type f -exec bash -c 'file=${1#./}; mv "$file" "${file//\//.}"' _ '{}' \;
+
+delete now-empty folders
+
+rm *.hs-boot
+rm *.hsc
+ghc -E * -I../../../../base-4.9.1.0/include -optP -P -optL -P
+rm *.hs
+rename 's/.hspp$/.hs/' *.hspp
+
+-}
+
 main :: IO ()
 main = do
 
