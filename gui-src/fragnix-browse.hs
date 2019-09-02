b4b111c70ba3d0965d46268098ba86c11af2d99d
Florian <mr.florian.kellner@web.de>
2019-09-02 18:01:21 +0200

    cleaning up

--- gui-src/fragnix-browse.hs
+++ gui-src/fragnix-browse.hs
@@ -6,31 +6,19 @@ module Main where
 
 import Prelude hiding (writeFile,readFile)
 
--- import Fragnix.HashLocalSlices (hashLocalSlices)
 import Fragnix.Slice (Slice)
 
 import Data.Aeson (eitherDecode,encode)
 import Data.Either (rights)
 import Data.ByteString.Lazy (writeFile,readFile)
--- import System.FilePath ((<.>))
-
--- import System.Environment (getArgs)
 
 import System.Directory (getCurrentDirectory, listDirectory, doesFileExist)
 import Control.Monad (forM, filterM)
 
 -- | Imports for the Servant Server
 import Control.Monad.IO.Class (liftIO)
-
-{-
-import Servant.API ((:>), Get, JSON)
-import Data.Proxy (Proxy(..))
--}
-
 import Servant -- (Server, Handler, serve, (:<|>))
 import Servant.Static.TH (createServerExp)
-
--- import Control.Monad.Trans.Either (EitherT)
 import Network.Wai.Handler.Warp (run)
 
 import Api
@@ -38,9 +26,8 @@ import Api
 staticServer :: Server StaticAPI
 staticServer = $(createServerExp "/home/florian/Documents/Sem6/fragnix-gui/fragnix/fragnix/gui-src/elm/dist")
 
--- | Start the API Server
-main :: IO ()
-main = run 8080 (serve api server)
+dynamicServer :: Server DynamicAPI
+dynamicServer = getSlicesHandler
 
 server :: Server API
 server
@@ -48,28 +35,13 @@ server
   = dynamicServer
   :<|> staticServer
 
-dynamicServer :: Server DynamicAPI
-dynamicServer = getSlicesHandler
-
-
-
-{-
--- | API Definition
-
-type API
-  -- GET /contents
-  = "contents" :> Get '[JSON] [Slice]
-
-server :: Server API
-server
-  -- GET /contents
-  = getSlicesHandler
-
-api :: Proxy API
-api = Proxy
--}
+-- | Start the API Server
+main :: IO ()
+main = run 8080 (serve api server)
 
 -- | GET /contents implementation
+getSlicesHandler :: Handler [Slice]
+getSlicesHandler = liftIO getSlices
 
 getSlices :: IO [Slice]
 getSlices = do
@@ -81,6 +53,3 @@ getSlices = do
   -- out if the tree is complete
   slices <- return (rights (map eitherDecode rawFiles))
   return slices
-
-getSlicesHandler :: Handler [Slice]
-getSlicesHandler = liftIO getSlices
