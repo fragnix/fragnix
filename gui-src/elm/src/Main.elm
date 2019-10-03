20730efceb23d2d17793032088734321f53399d5
Florian <mr.florian.kellner@web.de>
2019-10-03 16:18:39 +0200

    refactor EditorField and Color Palette into modules

--- gui-src/elm/src/Main.elm
+++ gui-src/elm/src/Main.elm
@@ -11,11 +11,11 @@ import Http
 import Slice exposing (..)
 import LocalSlice exposing (..)
 import TupleHelper exposing (..)
+import Palette exposing (..)
+import EditorField exposing (syntaxHighlight, inlineSH, editorField)
 
 -- imports for view
-import Parser exposing (Parser)
 import SyntaxHighlight as SH
-import SyntaxHighlight.Line as SHL
 import Html exposing (Html, button, div, text, p, h1, textarea)
 import Html.Attributes exposing (class, value, classList, spellcheck, readonly)
 import Html.Events exposing (onClick, on, onMouseEnter, onMouseLeave)
@@ -834,7 +834,7 @@ createHtml el =
         (Html.node
           "style"
           []
-          [ (Html.text (monokai_colors_css ++ show_caret_css ++ reference_css)) ])
+          [ (Html.text (EditorField.css)) ])
     , el
     ]
   |> Element.layoutWith { options = options } []
@@ -1142,94 +1142,3 @@ frameIf framed =
      [ Border.width 1, Border.color monokai_white ]
    else
      [ Border.width 1, Border.color monokai_black ]
-
-
--- | COLOR PALETTE
-monokai_black = (Element.rgb255 35 36 31)
-monokai_grey = (Element.rgb255 51 52 47)
-monokai_white = (Element.rgb255 247 247 241)
-actual_black = Element.rgb 0 0 0
-glass = Element.rgba 0 0 0 0
-orange = Element.rgb255 255 133 51
-edges =
-   { top = 0
-   , right = 0
-   , bottom = 0
-   , left = 0
-   }
-
-
--- | SYNTAX HIGHLIGHTING
-monokai_colors_css =
-  ".elmsh {color: #f8f8f2;}.elmsh-hl {background: #343434;}.elmsh-add {background: #003800;}.elmsh-del {background: #380000;}.elmsh-comm {color: #75715e;}.elmsh1 {color: #ae81ff;}.elmsh2 {color: #e6db74;}.elmsh3 {color: #f92672;}.elmsh4 {color: #66d9ef;}.elmsh5 {color: #a6e22e;}.elmsh6 {color: #ae81ff;}.elmsh7 {color: #fd971f;}.elmsh-elm-ts, .elmsh-js-dk, .elmsh-css-p {font-style: italic;color: #66d9ef;}.elmsh-js-ce {font-style: italic;color: #a6e22e;}.elmsh-css-ar-i {font-weight: bold;color: #f92672;}"
-show_caret_css =
-  "textarea {caret-color: white;}"
-reference_css =
-  ".reference {text-decoration: underline;}.reference:hover {cursor:pointer;}"
-
-
-type alias HighlightDict = Dict String (List (Html.Attribute Msg))
-
--- create a syntaxhighlighted, shrinkwrapped element
-syntaxHighlight : String -> HighlightDict -> Element Msg
-syntaxHighlight txt dict =
-  SH.haskell (if String.endsWith "\n" txt then txt ++ " " else txt)
-    |> Result.map (SH.dictAddAttributes dict)
-    |> Result.map SH.toInlineHtml
-    {- |> Result.map (\h ->
-        Html.div []
-            [ Html.node "style" [] [ (Html.text monokai_colors_css) ]
-            , h
-            ] ) -}
-    |> Result.map Element.html
-    -- padding to the right is needed to prevent overlayed text from breaking too early
-    |> Result.map (Element.el [ Element.paddingEach { edges | right = 3 } ])
-    |> Result.mapError Parser.deadEndsToString
-    |> (\result ->
-            case result of
-                Result.Ok a ->
-                    a
-
-                Result.Err x ->
-                    Element.text x
-       )
-
--- create inline HTML of code
-inlineSH : String -> Element Msg
-inlineSH txt =
-  SH.haskell txt
-  |> Result.map SH.toInlineHtml
-  |> Result.map Element.html
-  |> Result.mapError Parser.deadEndsToString
-  |> (\result ->
-        case result of
-          Ok a  -> a
-          Err x -> Element.text x)
-
--- create a syntaxhighlighted, shrinkwrapped textarea
-editorField : String -> (String -> Msg) -> HighlightDict -> Element Msg
-editorField txt onChange dict =
-    Element.el
-        [ Element.inFront (invisibleTextarea txt onChange)
-        ]
-        (syntaxHighlight txt dict)
-
-invisibleTextarea : String -> (String -> Msg) -> Element Msg
-invisibleTextarea txt onChange =
-  Input.multiline
-      [ Element.height Element.shrink
-      , Element.width Element.shrink
-      , Background.color glass
-      , Font.color glass
-      , Element.spacing 0
-      , Element.padding 0
-      , Border.rounded 0
-      , Border.width 0
-      , Border.glow glass 0.0
-      ]
-      { onChange = onChange
-      , text = txt
-      , placeholder = Nothing
-      , label = Input.labelHidden (String.left 5 txt)
-      , spellcheck = False
-      }
---
+++ gui-src/elm/src/Palette.elm
@@ -0,0 +1,17 @@
+module Palette exposing (..)
+
+import Element
+
+-- | COLOR PALETTE
+monokai_black = (Element.rgb255 35 36 31)
+monokai_grey = (Element.rgb255 51 52 47)
+monokai_white = (Element.rgb255 247 247 241)
+actual_black = Element.rgb 0 0 0
+glass = Element.rgba 0 0 0 0
+orange = Element.rgb255 255 133 51
+edges =
+   { top = 0
+   , right = 0
+   , bottom = 0
+   , left = 0
+   }
