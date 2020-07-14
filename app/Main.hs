c6089e96079a8b0ad48a132857879a064f88a02c
BinderDavid <binderd@informatik.uni-tuebingen.de>
2020-07-14 19:47:06 +0200

    Add --version option

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import Build (build)
+import Paths_fragnix
+import Data.Version (showVersion)
 
 import Options.Applicative (
   ParserInfo, Parser, execParser,
-  subparser, command, info,
+  subparser, command, info, infoOption, long, help,
   progDesc, header, metavar, helper, (<**>),
   many, argument, str, auto, fullDesc)
 
@@ -13,13 +15,18 @@ data Command = Build [FilePath]
 
 commandParserInfo :: ParserInfo Command
 commandParserInfo =
-  info (commandParser  <**> helper) (fullDesc <> header "fragnix - fragment-based code distribution")
+  info (commandParser  <**> helper <**> versionOption) (fullDesc <> header "fragnix - fragment-based code distribution")
 
 commandParser :: Parser Command
 commandParser = subparser (mconcat [
   command "build" (info (buildParser <**> helper) (
     progDesc "Build the given list of modules."))
   ])
+
+versionOption :: Parser (a -> a)
+versionOption = infoOption versionText (long "version" <> help "Show version")
+  where
+    versionText = "Version " <> showVersion version
 
 buildParser :: Parser Command
 buildParser = Build <$> many (argument str (metavar "TARGET"))
