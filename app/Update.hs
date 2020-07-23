f7a7b1d82ef61a3cb54aa34f4ab3f7b1a05d20b7
BinderDavid <binderd@informatik.uni-tuebingen.de>
2020-07-23 21:23:00 +0200

    Add inspect function

--- app/Update.hs
+++ app/Update.hs
@@ -9,8 +9,9 @@ import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
 import Control.Monad (forM_)
-import Fragnix.Slice (SliceID)
+import Fragnix.Slice (SliceID, readSlice, Slice(..),Fragment(..))
 import Fragnix.Update (getUpdates, PersistedUpdate(..), UpdateID, applyUpdate, createUpdate, writeUpdate, findUpdateFuzzy)
+import Fragnix.Paths (slicesPath)
 
 import Options.Applicative (
   ParserInfo, Parser, execParser,
@@ -22,6 +23,7 @@ data UpdateCommand
   = List
   | Create Text SliceID SliceID
   | Apply UpdateID
+  | Inspect UpdateID
 
 update :: UpdateCommand -> IO ()
 update List = do
@@ -38,13 +40,29 @@ update (Apply desc) = do
   case maybeUpdate of
     Nothing -> putStrLn "Couldn't find an update matching the description."
     Just upd -> applyUpdate upd
-
+update (Inspect desc) = do
+  maybeUpdate <- findUpdateFuzzy desc
+  case maybeUpdate of
+    Nothing -> putStrLn "Couldn't find an update matching the description."
+    Just upd -> do
+      Text.putStrLn $ "Description: " <> updateDescription upd
+      Text.putStrLn $ "Hash:        " <> updateID upd
+      Text.putStrLn $ " ---"
+      forM_ (updateContent upd) $ \(sliceID, sliceID') -> do
+        Text.putStrLn $ "Replacing content from slice: " <> sliceID <> "\n"
+        (Slice _ _ (Fragment code) _ _) <- readSlice slicesPath sliceID
+        Text.putStrLn (Text.unlines code)
+        Text.putStrLn $ "By content from slice:        " <> sliceID' <> "\n"
+        (Slice _ _ (Fragment code') _ _) <- readSlice slicesPath sliceID'
+        Text.putStrLn (Text.unlines code')
+        Text.putStrLn $ " ---"
 
 updateParser :: Parser UpdateCommand
 updateParser = subparser (mconcat [
     command "list"   (info (listParser   <**> helper) (progDesc "List all available updates.")),
     command "create" (info (createParser <**> helper) (progDesc "Create a new update.")),
-    command "apply"  (info (applyParser  <**> helper) (progDesc "Apply the specified update."))])
+    command "apply"  (info (applyParser  <**> helper) (progDesc "Apply the specified update.")),
+    command "inspect"  (info (inspectParser  <**> helper) (progDesc "Inspect the specified update."))])
   where
     listParser = pure List
     createParser = Create
@@ -52,3 +70,4 @@ updateParser = subparser (mconcat [
       <*> argument str (metavar "SLICEID")
       <*> argument str (metavar "SLICEID")
     applyParser = Apply <$> argument str (metavar "ID/DESC")
+    inspectParser = Inspect <$> argument str (metavar "ID/DESC")
