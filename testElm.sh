a8c77c86c943473888f16e8722d41e938d4e64ac
BinderDavid <binderd@informatik.uni-tuebingen.de>
2021-03-24 19:18:42 +0100

    Remove gui from master

--- testElm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# compile and serve fragnix frontend
-PATH=$PATH:$(pwd)/.cabal-sandbox/bin
-cd gui-src/elm && ./optimize.sh && cd ../.. && cabal install && cd tests/packages/application && fragnix-browse
