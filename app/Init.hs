cb19e846c3e2da9c67b1699a458f0e3891b2c3e3
Dennis Hieber <dennis@rm-hieber.de>
2021-04-07 10:29:59 +0200

    print information to console while fetching

--- app/Init.hs
+++ app/Init.hs
@@ -8,11 +8,13 @@ import Utils (archiveRequest, extract)
 import Network.HTTP.Req (responseBody)
 import System.Directory (createDirectoryIfMissing)
 import Control.Monad (forM_)
+import Data.Text (pack)
 
 init :: IO ()
 init = do
   createDirectoryIfMissing True fragnixBasePath
   createDirectoryIfMissing True environmentPath
-  forM_ [("builtin.tar.gz", builtinEnvironmentPath), ("cbits.tar.gz", cbitsPath), ("include.tar.gz", includePath)] (\(archive, path) -> do
-    r <- archiveRequest archive
+  forM_ [("builtin", builtinEnvironmentPath), ("cbits", cbitsPath), ("include", includePath)] (\(archive, path) -> do
+    putStrLn $ "Fetching " ++ archive ++ " ..."
+    r <- archiveRequest . pack $ archive ++ ".tar.gz"
     extract path $ responseBody r)
