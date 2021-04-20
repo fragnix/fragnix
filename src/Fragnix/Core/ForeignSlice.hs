9650f1c042a47a32f1cfed7dc5003e945565057c
Dennis Hieber <dennis@rm-hieber.de>
2021-04-20 10:45:18 +0200

    add foreign slice definition

---
+++ src/Fragnix/Core/ForeignSlice.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE DeriveGeneric, StandaloneDeriving #-}
+module Fragnix.Core.ForeignSlice where
+
+import Fragnix.Core.Slice (SliceID)
+
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Text (Text)
+import GHC.Generics (Generic)
+
+data ForeignSlice = ForeignSlice
+  { sliceID   :: SliceID
+  , code      :: Text
+  , fileType  :: Text
+  }
+
+deriving instance Show ForeignSlice
+deriving instance Eq ForeignSlice
+deriving instance Ord ForeignSlice
+deriving instance Generic ForeignSlice
+instance ToJSON ForeignSlice
+instance FromJSON ForeignSlice
