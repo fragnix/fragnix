adb435e95fea908402354c01e4f43aef1fbb5a26
denhie <dennis@rm-hieber.de>
2021-04-28 13:55:10 +0200

    Copy basket definition from env-type

--- src/Fragnix/Basket.hs
+++ src/Fragnix/Basket.hs
@@ -1,27 +1,43 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Fragnix.Basket where
 
 import Prelude hiding (readFile, writeFile)
 
 import Fragnix.Core.Basket (Basket)
-import Fragnix.Loaf (loafToSymbols, pickLoaf, symbolsToLoaf)
+import Fragnix.Core.Loaf (Loaf (..))
+import Fragnix.Loaf (loafToSymbols, pickLoaf, readLoafFile)
 
-import Control.Monad (forM)
-import qualified Data.Map as Map (empty, foldrWithKey, fromList, insert, toList)
-import Data.Text (pack, unpack)
+import Control.Monad (filterM, forM)
+import Data.Aeson.Encode.Pretty (encodePretty)
+import Data.ByteString.Lazy (readFile, writeFile)
+import qualified Data.Map as Map (fromList, toList)
+import Data.Text (unpack)
 import Language.Haskell.Exts.Syntax (ModuleName (..))
-import Language.Haskell.Names (Environment, Symbol)
+import Language.Haskell.Names (Environment)
+import System.Directory
+    (createDirectoryIfMissing, doesFileExist, getDirectoryContents)
+import System.FilePath ((</>))
 
 
 basketToEnvironment :: Basket -> IO Environment
 basketToEnvironment b = do
   envList <- forM (Map.toList b) (\(name, loaves) -> do
-    loaf <- pickLoaf loaves
-    symbols <- loafToSymbols loaf
-    return (ModuleName () (unpack name), symbols))
+  loaf <- pickLoaf loaves
+  symbols <- loafToSymbols loaf
+  return (ModuleName () (unpack name), symbols))
   return $ Map.fromList envList
 
-environmentToBasket :: Environment -> Basket
-environmentToBasket = Map.foldrWithKey convertToBasket Map.empty
+readBasket :: FilePath -> IO Basket
+readBasket path = do
+  createDirectoryIfMissing True path
+  filenames <- getDirectoryContents path
+  let pathmodulnames = map (path </>) filenames
+  existingPathModulNames <- filterM doesFileExist pathmodulnames
+  fmap Map.fromList (forM existingPathModulNames (\file -> do
+    Loaf {name, loafID}<- readLoafFile file
+    return (name,[loafID])))
 
-convertToBasket :: ModuleName () -> [Symbol] -> Basket -> Basket
-convertToBasket (ModuleName () name) symbols = undefined
+writeBasket :: FilePath -> Basket -> IO ()
+writeBasket path b = do
+  createDirectoryIfMissing True path
+  writeFile path (encodePretty b)
